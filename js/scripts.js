2194a4aecb45aa2149d28702876aa6b1b6eaa593
Florence <mbababziflorence81@gmail.com>
2018-12-18 11:47:58 +0200

    jquerie

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function() {
+  jQuery("hi").click(function() {
+    alert("This is a header.");
+  });
+
+  jQuery("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  jQuery("img").click(function() {
+    alert("This is an image.");
+  });
+});
